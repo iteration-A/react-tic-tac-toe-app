a9f65b8cccc2fd1e851c958592b4862374218916
Manuel Luna <manuellunada@gmail.com>
2020-06-22 23:52:59 -0500

    Add transition between UsernameFormPrompt and HomePage's content

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,25 +1,36 @@
 import React from 'react';
+import { CSSTransition } from 'react-transition-group';
 import UsernameFormPrompt from 'components/UsernameFormPrompt';
 import useInputForm from 'hooks/useInputForm';
 import classes from 'components/HomePage.module.css';
 import { content, container, btn } from 'shared/styles.module.css';
+import 'shared/transitions.css';
 
 export default function SignIn() {
   const [username, setUsername] = useInputForm();
 
-  return (
-    <>
-      {/* if username is not defined then render UsernameFormPrompt */}
-      {username || <UsernameFormPrompt updateInput={setUsername} />}
-      <div className={container}>
-        <div className={content}>
-          <h1 className={classes.title}>Welcome, {username}</h1>
-          <div className={classes['btn-container']}>
-            <button className={`${btn} ${classes['homepage-btn']}`}>New game</button>
-            <button className={`${btn} ${classes['homepage-btn']}`}>Join game</button>
-          </div>
+  const UsernameFormPromtComponent = (
+    <CSSTransition in={username === ''} timeout={1000} classNames="slide-up" unmountOnExit>
+      <UsernameFormPrompt updateInput={setUsername} />
+    </CSSTransition>
+  );
+
+  const HomePageContent = (
+    <div className={container}>
+      <div className={content}>
+        <h1 className={classes.title}>Welcome, {username}</h1>
+        <div className={classes['btn-container']}>
+          <button className={`${btn} ${classes['homepage-btn']}`}>New game</button>
+          <button className={`${btn} ${classes['homepage-btn']}`}>Join game</button>
         </div>
       </div>
+    </div>
+  );
+
+  return (
+    <>
+      {UsernameFormPromtComponent}
+      {HomePageContent}
     </>
   );
 }
